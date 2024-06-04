3d5464d404798e0427ed8403016c6690ad4d19b8
Tanmay Jain <123488621+Tanmay-Jain-GitHUB@users.noreply.github.com>
2024-06-04 23:26:06 +0530

    Add files via upload

---
+++ bgColorChanger.js
@@ -0,0 +1,17 @@
+const body = document.querySelector('body');
+        const buttons = document.querySelectorAll('.button');
+        buttons.forEach(function (button){
+            button.addEventListener('click', function(e){
+                if(e.target.id === 'grey')
+                    body.style.backgroundColor = e.target.id;
+                if(e.target.id === 'blue')
+                    body.style.backgroundColor = e.target.id;
+                if(e.target.id === 'yellow')
+                    body.style.backgroundColor = e.target.id;
+                if(e.target.id === 'greenyellow')
+                    body.style.backgroundColor = e.target.id;
+                if(e.target.id === 'red')
+                    body.style.backgroundColor = e.target.id;
+                
+            })
+        });
